96a97af81847ed4863ce342e599fb670ea7237bc
Marcos Tromel <marcos.tromel@gmail.com>
2024-10-12 10:22:58 -0300

    adicionado favicon, validador de png e redimensionamento de imagem

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -68,6 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'site_setup.context_processor.context_processor_example',
+                'site_setup.context_processor.site_setup',
             ],
         },
     },
